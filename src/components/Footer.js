e59355d11d75757675c2ff3b17617a1811d4c6ff
Khanjan <khanjan@example.com>
2025-08-14 14:33:14 +0530

    changed some styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,14 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-secondary text-center py-6 border-t mt-12">
-      <p className="text-sm text-gray-600">© Designed By <b><a href="/">Khanjan Gadhiya.</a></b></p>
+      <p className="text-sm text-gray-600">© Designed By <b><a 
+        href="/" 
+        onClick={(e) => {
+              e.preventDefault();
+              document.getElementById('home').scrollIntoView({behavior: "smooth"});
+            }} >
+              Khanjan Gadhiya.
+            </a></b></p>
     </footer>
   );
 };
